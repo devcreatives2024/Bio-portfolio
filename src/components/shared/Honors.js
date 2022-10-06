93e22be3691d938b12cff34e971e10338587bf91
hayatul-islam <hayatulemon340@gmail.com>
2022-10-06 21:22:34 +0600

    honour design

---
+++ src/components/shared/Honors.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Honors() {
+  return (
+    <>
+      <div className="bg-[#d1d3e0] ">
+        <div className="grid content-between justify-center font-medium  h-[180px] pt-2 pb-8">
+          <h1 className="text-2xl text-center">W.</h1>
+          <h1 className="rotate-[-90deg]">Honors</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Honors;
--- src/pages/TheLayout.js
+++ src/pages/TheLayout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../components/shared/header/Header";
+import Honors from "../components/shared/Honors";
 import Home from "./Home";
 
 function TheLayout() {
@@ -8,6 +9,9 @@ function TheLayout() {
     <>
       <BrowserRouter>
         <Header />
+        <div className="fixed top-[50%] translate-y-[-50%] ">
+          <Honors />
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
